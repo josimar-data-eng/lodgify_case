8e2f649ab7c42bfd7446845ec7f1259695efedc9
Josimar Junior <josimar_junior@id.uff.br>
2023-02-28 23:25:28 -0300

    first commit

---
+++ sandbox/months_since_first_subscription.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import operations as ops
+from IPython.display import display
+
+
+
+def generate_months_passed_since_first_sub(path_in,path_out,file_in,last_book_file_in,first_sub_file_in,file_out):
+    #---------------------#
+    #- Reading stg files -# To refactor
+    #---------------------#
+
+    stg_subscription_df   = pd.read_csv(path_in+file_in          , parse_dates=['sub_date'])
+    last_booking_df       = pd.read_csv(path_in+last_book_file_in, parse_dates=['last_booking_date'])
+    first_subscription_df = pd.read_csv(path_in+first_sub_file_in, parse_dates=['first_subscription_date'])
+
+
+    #--------------------------------------------------------------------#
+    #- How many months has passed since their first subscription month? -#
+    #--------------------------------------------------------------------#
+
+    #----- According to the booking table -----#
+    months_since_first_sub_df = last_booking_df\
+                                            .merge(\
+                                                  first_subscription_df
+                                                , on='subscriber_id'
+                                                , how='left'
+                                            )
+
+    ops.date_diff_months(  months_since_first_sub_df
+                        , 'first_subscription_date'
+                        , 'last_booking_date'
+                        , 0
+                        , "months_since_first_sub"
+                        , False
+                      )
+
+    months_since_first_sub_df.drop(
+                                    labels=[ 'last_booking_date'
+                                            ,'first_subscription_date'
+                                          ]
+                                    , axis=1
+                                    , inplace=True
+                                )
+    new_file_out = (file_out.replace((file_out[len(file_out) - 4:]),'')+"_approach1"+file_out[len(file_out) - 4:])
+    months_since_first_sub_df.to_csv(path_out+new_file_out)
+
+    #----- According to the subscription table -----#
+    stg_subscription_df.rename(
+                                columns={
+                                        'sub_id':'subscriber_id'
+                                        }
+                                ,inplace=True
+                            )
+    
+    months_since_first_sub_df = stg_subscription_df\
+                                                .merge(\
+                                                      first_subscription_df
+                                                    , on='subscriber_id'
+                                                    , how='left'
+                                                )
+
+    ops.date_diff_months(  months_since_first_sub_df
+                        , 'first_subscription_date'
+                        , 'sub_date'
+                        , 0
+                        , "months_since_first_sub"
+                        , True
+                        )
+
+    months_since_first_sub_df = months_since_first_sub_df[
+                                                          ['subscriber_id'
+                                                          ,'sub_year_month'
+                                                          ,'months_since_first_sub']
+                                                        ]
+    new_file_out = (file_out.replace((file_out[len(file_out) - 4:]),'')+"_approach2"+file_out[len(file_out) - 4:])
+    months_since_first_sub_df.to_csv(path_out+new_file_out)
+
+    
---
+++ staging/stg_treatment.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import operations as ops
+from IPython.display import display
+
+
+def treat_stg_booking(path_in_out,file_in,first_sub_file,last_book_file):
+
+    #----------------------#
+    #--------BOOKING-------#
+    #----------------------#
+
+    #-------------------------------------------------#
+    # Getting the first subscription date per each id #
+    #-------------------------------------------------#
+
+    #Should the first subscription be in the booking table? I'm not sure, but I can't get the first
+    #subscription from the subscription table because of it's a snapshot with the last day of every month
+    #The first_subscription table is created to calculate how many months has passes since the 1o subscription that will be loaded in the sandbox layer.
+    columns = ['subscriber_id','booking_status','booking_date','booking_year_month','month','date']
+    stg_booking_df = pd.read_csv(path_in_out+file_in)[columns]
+
+    stg_booking_df = stg_booking_df\
+                                .query\
+                                    (
+                                    "booking_status not in ('Canceled','')"
+                                    )
+    
+    #Getting the first date per id and defining it as first_subscription_date
+    first_subscription_df = ops.groupby(  stg_booking_df
+                                        , ['subscriber_id']
+                                        , 'booking_date'
+                                        , "min"
+                                        , "first_subscription_date"
+                                    )
+    first_subscription_df.to_csv(path_in_out+first_sub_file)
+    
+    #-------------------------------------------#
+    # Getting the last booking date per each id #
+    #-------------------------------------------#    
+
+    # The last_booking table will be used to compare with fist_subscription table to get the months passed since 1o subscription according the bookin table point of view.
+    # Do I need to filter the satus? Is the subscription effective only if the status is not canceled?
+    last_booking_df = ops.groupby( stg_booking_df
+                                , ['subscriber_id'
+                                , 'booking_year_month']
+                                , 'booking_date'
+                                , "max"
+                                , "last_booking_date"
+                            )
+    last_booking_df.to_csv(path_in_out+last_book_file)
+
+    
