08768cd5c4845ed0b67e04219a8ecd9e4074fdfd
Josimar Junior <josimar_junior@id.uff.br>
2023-03-01 11:24:34 -0300

    code indentation && renaming sandbox files and columns

--- sandbox/months_active_canceled.py
+++ sandbox/months_active_canceled.py
@@ -2,54 +2,44 @@ import pandas as pd
 from IPython.display import display
 
 
-def generate_months_per_status(path_in,path_out,file_in,file_out):
+def get_months_per_status(path_in, path_out, file_in, file_out):
+    # ------------------------------------------------------------#
+    # - How many months they were an active/canceled subscriber? -#
+    # ------------------------------------------------------------#
 
-    #------------------------------------------------------------#
-    #- How many months they were an active/canceled subscriber? -#
-    #------------------------------------------------------------#
+    # -- Reading stg file --#
+    stg_subscription_df = pd.read_csv(path_in + file_in)
 
-
-    #-- Reading stg file --#
-    stg_subscription_df = pd.read_csv(path_in+file_in)
-
-    #Selecting only necessary columns
-    count_status_df = stg_subscription_df\
-                                    .filter(
-                                            ['sub_id','status','sub_year_month']
-                                            )
+    # Selecting only necessary columns
+    count_status_df = stg_subscription_df.filter(["sub_id", "status", "sub_year_month"])
 
     # iterating throug dataframe to create a numeric column that will be used to sum over operation
     for index, row in count_status_df.iterrows():
-        if row['status'].lower().strip() == 'active':
-            count_status_df.at[index,'is_active'] = 1
-            count_status_df.at[index,'is_canceled'] = 0
+        if row["status"].lower().strip() == "active":
+            count_status_df.at[index, "is_active"] = 1
+            count_status_df.at[index, "is_canceled"] = 0
         else:
-            count_status_df.at[index,'is_active'] = 0
-            count_status_df.at[index,'is_canceled'] = 1
+            count_status_df.at[index, "is_active"] = 0
+            count_status_df.at[index, "is_canceled"] = 1
 
+    count_status_df.sort_values(
+        by=["sub_id", "sub_year_month"], ascending=[True, True], inplace=True
+    )
 
-    count_status_df.sort_values(by =  ['sub_id', 'sub_year_month']
-                                    , ascending = [True, True]
-                                    , inplace=True
-                                )
-    
-    count_status_df['qtt_monhts_active'] = count_status_df\
-                                                    .groupby(
-                                                            ['sub_id']
-                                                            )\
-                                                    ['is_active'].cumsum().astype(int)
+    count_status_df["qtt_monhts_active"] = (
+        count_status_df.groupby(["sub_id"])["is_active"].cumsum().astype(int)
+    )
 
-    count_status_df['qtt_monhts_canceled'] = count_status_df\
-                                                    .groupby(
-                                                            ['sub_id']
-                                                            )\
-                                                    ['is_canceled'].cumsum().astype(int)
+    count_status_df["qty_monhts_canceled"] = (
+        count_status_df.groupby(["sub_id"])["is_canceled"].cumsum().astype(int)
+    )
 
-    count_status_df = count_status_df\
-                                .filter(
-                                        ['sub_id','sub_year_month','qtt_monhts_active','qtt_monhts_canceled']
-                                        )
-    count_status_df.to_csv(path_out+file_out)
-    print(f"File {file_out} loaded at {path_out} directory with {len(count_status_df.index)} rows.")        
+    count_status_df = count_status_df.filter(
+        ["sub_id", "sub_year_month", "qtt_monhts_active", "qty_monhts_canceled"]
+    )
+    count_status_df.to_csv(path_out + file_out)
+    print(
+        f"File {file_out} loaded at {path_out} directory with {len(count_status_df.index)} rows."
+    )
 
     # display(stg_subscription_df)
--- sandbox/months_since_last_status_change.py
+++ sandbox/months_since_last_status_change.py
@@ -4,106 +4,97 @@ from IPython.display import display
 from pandas.tseries.offsets import DateOffset
 
 
-def generate_months_status_change(path_in,path_out,file_in,file_out):
+def get_months_status_change(path_in, path_out, file_in, file_out):
+    # ---------------------------------------------------------------------------#
+    # - How many months has passed since their last subscription status change? -#
+    # ---------------------------------------------------------------------------#
 
-    #---------------------------------------------------------------------------#
-    #- How many months has passed since their last subscription status change? -#
-    #---------------------------------------------------------------------------#
-
-    stg_subscription_df = pd.read_csv(path_in+file_in, parse_dates=['sub_date'])
+    stg_subscription_df = pd.read_csv(path_in + file_in, parse_dates=["sub_date"])
     # stg_subscription_df = pd.read_csv("staging/files/subscription.csv", parse_dates=['sub_date'])
 
+    months_status_change_df = stg_subscription_df.filter(
+        ["sub_id", "status", "sub_year_month", "sub_date"]
+    )
+    months_status_change_df.sort_values(
+        by=["sub_id", "sub_year_month"], ascending=[True, True], inplace=True
+    )
 
-    months_status_change_df = stg_subscription_df\
-                                            .filter(
-                                                    [ 'sub_id'
-                                                    , 'status'
-                                                    , 'sub_year_month'
-                                                    , 'sub_date']
-                                                )
-    months_status_change_df.sort_values(by = ['sub_id', 'sub_year_month'], ascending = [True, True],inplace=True)
+    ops.positional_function(
+        "lag", months_status_change_df, "status", "previous_status", "sub_id"
+    )
+    ops.positional_function(
+        "lag", months_status_change_df, "sub_date", "previous_sub_date", "sub_id"
+    )
 
-    ops.positional_function('lag' 
-                            ,months_status_change_df
-                            ,'status'  
-                            ,'previous_status'  
-                            ,'sub_id' 
-                            )
-    ops.positional_function('lag' 
-                            ,months_status_change_df
-                            ,'sub_date'
-                            ,'previous_sub_date'
-                            ,'sub_id'
-                            )
-
-
-    #option 1: Marking if it was status change
-    for index, row in months_status_change_df.iterrows():        
-        if row['previous_status'] != '':
-            if row['status'] != row['previous_status']:
-                months_status_change_df.at[index,'idc_status_change'] = 1
+    # option 1: Marking if it was status change
+    for index, row in months_status_change_df.iterrows():
+        if row["previous_status"] != "":
+            if row["status"] != row["previous_status"]:
+                months_status_change_df.at[index, "idc_status_change"] = 1
             else:
-                months_status_change_df.at[index,'idc_status_change'] = 0
+                months_status_change_df.at[index, "idc_status_change"] = 0
         else:
-            months_status_change_df.at[index,'idc_status_change'] = 0
-    months_status_change_df['idc_status_change'] = months_status_change_df['idc_status_change'].astype(int)
+            months_status_change_df.at[index, "idc_status_change"] = 0
+    months_status_change_df["idc_status_change"] = months_status_change_df[
+        "idc_status_change"
+    ].astype(int)
 
     only_status_change_df = months_status_change_df.query("idc_status_change == 1")
-    
-    first_status_change_df = ops.groupby( only_status_change_df
-                                        , ['sub_id']
-                                        , 'sub_date'
-                                        , 'min'
-                                        , 'first_status_change'
-                                        )
-    first_status_change_dict = first_status_change_df.to_dict('dict')
 
+    first_status_change_df = ops.groupby(
+        only_status_change_df, ["sub_id"], "sub_date", "min", "first_status_change"
+    )
+    first_status_change_dict = first_status_change_df.to_dict("dict")
 
     for index, row in months_status_change_df.iterrows():
         for values in first_status_change_dict.values():
             for i in values:
-                if row['sub_id'] == i and row['sub_date'] == values[i]:
-                    months_status_change_df.at[index,'first_status_change'] = True
+                if row["sub_id"] == i and row["sub_date"] == values[i]:
+                    months_status_change_df.at[index, "first_status_change"] = True
 
     # To refactor, because this specific value can't be filled
-    ops.positional_function('lag'
-                            ,months_status_change_df
-                            ,'idc_status_change'
-                            ,'previous_idc_status_change'
-                            ,'sub_id' 
-                            )
+    ops.positional_function(
+        "lag",
+        months_status_change_df,
+        "idc_status_change",
+        "previous_idc_status_change",
+        "sub_id",
+    )
 
     for index, row in months_status_change_df.iterrows():
-        if row['first_status_change'] == True:
-            last_change = row['sub_date']
-            months_status_change_df.at[index,'last_change'] = last_change
+        if row["first_status_change"] == True:
+            last_change = row["sub_date"]
+            months_status_change_df.at[index, "last_change"] = last_change
 
-        else: # If had change in the previous row
-            if row['previous_idc_status_change'] == 1:
-                last_change = row['previous_sub_date']
-                months_status_change_df.at[index,'last_change'] = last_change
+        else:  # If had change in the previous row
+            if row["previous_idc_status_change"] == 1:
+                last_change = row["previous_sub_date"]
+                months_status_change_df.at[index, "last_change"] = last_change
 
     for index, row in months_status_change_df.iterrows():
-        if (row['previous_idc_status_change']) == 0 and row['first_status_change'] != True:
-            months_status_change_df.loc[index,'last_change'] = months_status_change_df.loc[index-1,'last_change']
+        if (row["previous_idc_status_change"]) == 0 and row[
+            "first_status_change"
+        ] != True:
+            months_status_change_df.loc[
+                index, "last_change"
+            ] = months_status_change_df.loc[index - 1, "last_change"]
 
     for index, row in months_status_change_df.iterrows():
         for values in first_status_change_dict.values():
             for i in values:
-                if row['sub_id'] == i and row['sub_date'] < values[i]:
-                    months_status_change_df.at[index,'last_change'] = row['sub_date']
+                if row["sub_id"] == i and row["sub_date"] < values[i]:
+                    months_status_change_df.at[index, "last_change"] = row["sub_date"]
 
-    ops.date_diff_months(months_status_change_df,'last_change','sub_date',0,'qtt_months',True)
+    ops.date_diff_months(
+        months_status_change_df, "last_change", "sub_date", 0, "qty_months_since_last_change", True
+    )
 
-    months_status_change_df = months_status_change_df\
-                                                .filter(
-                                                        ['sub_id'
-                                                        ,'status'
-                                                        ,'sub_year_month'
-                                                        ,'qtt_months'
-                                                        ]
-                                                    )
+    months_status_change_df = months_status_change_df.filter(
+        ["sub_id", "status", "sub_year_month", "qty_months_since_last_change"]
+    )
 
     # display(months_status_change_df)
-    months_status_change_df.to_csv(path_out+file_out)
-    print(f"File {file_out} loaded at {path_out} directory with {len(months_status_change_df.index)} rows.")
+    months_status_change_df.to_csv(path_out + file_out)
+    print(
+        f"File {file_out} loaded at {path_out} directory with {len(months_status_change_df.index)} rows."
+    )
